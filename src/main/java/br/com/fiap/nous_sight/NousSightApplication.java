016fb48b9650942cc11c7530a02793c53b605b86
Thomas Sievers <t.sievers07@gmail.com>
2026-09-13 00:04:48 -0300

    Project setup

---
+++ src/main/java/br/com/fiap/nous_sight/NousSightApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.nous_sight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NousSightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NousSightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/nous_sight/NousSightApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.nous_sight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NousSightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
